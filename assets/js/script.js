52d8c02ad4b8a1f84dae8bc8da6bbb4ef34bdf77
Angela Man <angela.k.man@gmail.com>
2021-04-04 19:37:25 -0500

    complete main page html and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,14 @@
 // // update the countdown every 1 second
 // var timer = setInterval(function() {
 
-//     while (countDownTimer > 0) {
+//     while (countDownTimer >= 0) {
 //         countDownTimer--;
 //         // Display the result in the element with id timer
 //         document.getElementById("timer").innerHTML = countDownTimer;
 //     }
 
 //     // if the countdown is finished, let user know
-//     if (countDownTimer === 0) {
+//     if (countDownTimer < 0) {
 //         clearInterval(timer);
 //         document.getElementById("timer").innerHTML = "TIME'S UP!";
 //     }
